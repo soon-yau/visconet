46e9f7f0aaa0873233704338ccb2f9e3208e75b6
Soon Cheong <soonyau@gmail.com>
2024-02-12 10:31:14 +0000

    tidy up

--- visconet/visconet.py
+++ visconet/visconet.py
@@ -13,17 +13,18 @@ from ldm.models.diffusion.ddim import DDIMSampler
 
 class ViscoNetLDM(LatentDiffusion):
 
-    def __init__(self, control_stage_config, control_key, only_mid_control, control_cond_config, control_crossattn_key, mask_key=None,
-                 *args, **kwargs):
+    def __init__(self, control_stage_config, control_key, only_mid_control, control_cond_config, 
+                 control_crossattn_key, mask_key=None, enable_mask=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.control_model = instantiate_from_config(control_stage_config)
         self.control_key = control_key
         self.control_crossattn_key = control_crossattn_key
         self.only_mid_control = only_mid_control
         self.control_scales = [1.0] * 13
-        self.mask_enables = [1] * 13
+        self.enable_mask = enable_mask
+        self.mask_enables = [1 if enable_mask else 0] * 13
         self.mask_key = mask_key
-
+        self.ddim_sampler = DDIMSampler(self)
         # new
         self.control_cond_model = instantiate_from_config(control_cond_config)
 
@@ -78,7 +79,6 @@ class ViscoNetLDM(LatentDiffusion):
             def mask_control(c, mask_enable):
                 if mask_enable:
                     resized_mask = T.Resize(list(c.shape[-2:]), T.InterpolationMode.NEAREST)(cond_mask)
-                    #return c * resized_mask.unsqueeze(1)
                     return c * resized_mask
                 else: 
                     return c
@@ -94,19 +94,16 @@ class ViscoNetLDM(LatentDiffusion):
         return self.get_learned_conditioning([""] * N)
 
     @torch.no_grad()
-    def log_images(self, batch, N=4, n_row=2, sample=False, ddim_steps=20, ddim_eta=0.0, return_keys=None,
-                   quantize_denoised=True, inpaint=True, plot_denoise_rows=False, plot_progressive_rows=True,
-                   plot_diffusion_rows=False, unconditional_guidance_scale=9.0, unconditional_guidance_label=None,
-                   use_ema_scope=True,
-                   **kwargs):
+    def log_images(self, batch, N=4, n_row=2, sample=False, ddim_steps=20, ddim_eta=0.0, 
+                   plot_denoise_rows=False, plot_diffusion_rows=False, unconditional_guidance_scale=12.0,**kwargs):
         use_ddim = ddim_steps is not None
 
         log = dict()
         z, c = self.get_input(batch, self.first_stage_key, bs=N)
-        c_cat, c, c_text, mask = c["c_concat"][0][:N], c["c_crossattn"][0][:N], c["c_text"][0][:N], c["c_concat_mask"][0][:N]
         N = min(z.shape[0], N)
         n_row = min(z.shape[0], n_row)
-        reconstructed = self.decode_first_stage(z)
+        c_cat, c, c_text, mask = c["c_concat"][0][:N], c["c_crossattn"][0][:N], c["c_text"][0][:N], c["c_concat_mask"][0][:N]                
+        reconstructed = self.decode_first_stage(z)[:N]
         #log["reconstruction"] = self.decode_first_stage(z)
         log["control"] = c_cat * 2.0 - 1.0
         log["conditioning"] = log_txt_as_img((64, 64), batch[self.cond_stage_key], size=16)
@@ -129,7 +126,7 @@ class ViscoNetLDM(LatentDiffusion):
             diffusion_grid = make_grid(diffusion_grid, nrow=diffusion_row.shape[0])
             log["diffusion_row"] = diffusion_grid
 
-        n_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, cartoon'
+        n_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, sunglasses, hat'
 
         cond = {"c_concat": [c_cat], 
                 "c_crossattn": [c],
@@ -139,7 +136,7 @@ class ViscoNetLDM(LatentDiffusion):
         un_cond = {"c_concat": [c_cat], 
                 "c_crossattn": [torch.zeros_like(c)],
                 "c_text":[self.get_learned_conditioning([n_prompt] * N)],
-                'c_concat_mask': [mask] }
+                'c_concat_mask': [torch.zeros_like(mask)] }
         
         if sample:
             # get denoise row
@@ -147,7 +144,7 @@ class ViscoNetLDM(LatentDiffusion):
                                                      batch_size=N, ddim=use_ddim,
                                                      ddim_steps=ddim_steps, eta=ddim_eta)
             x_samples = self.decode_first_stage(samples)
-            log["samples"] = x_samples
+            #log["samples"] = x_samples
             if plot_denoise_rows:
                 denoise_grid = self._get_denoise_row_from_list(z_denoise_row)
                 log["denoise_row"] = denoise_grid
@@ -165,16 +162,17 @@ class ViscoNetLDM(LatentDiffusion):
                                              )
             x_samples_cfg = self.decode_first_stage(samples_cfg)
             #log[f"samples_cfg_scale_{unconditional_guidance_scale:.2f}"] = x_samples_cfg
+            log["samples"] = x_samples_cfg
             log['concat'] = torch.cat((reconstructed, x_samples_cfg), dim=-2)
 
         return log
 
     @torch.no_grad()
     def sample_log(self, cond, batch_size, ddim, ddim_steps, **kwargs):
-        ddim_sampler = DDIMSampler(self)
+
         b, c, h, w = cond["c_concat"][0].shape
         shape = (self.channels, h // 8, w // 8)
-        samples, intermediates = ddim_sampler.sample(ddim_steps, batch_size, shape, cond, verbose=False, **kwargs)
+        samples, intermediates = self.ddim_sampler.sample(ddim_steps, batch_size, shape, cond, verbose=False, **kwargs)
         return samples, intermediates
 
     @torch.no_grad()
@@ -190,7 +188,8 @@ class ViscoNetLDM(LatentDiffusion):
         for root_name in [sample_root, gt_root, src_root, concat_root]:
             os.makedirs(str(root_name), exist_ok=True)        
         # inference
-        images = self.log_images(batch, N=len(batch), ddim_steps=20, unconditional_guidance_scale=9.0)
+        images = self.log_images(batch, N=len(batch), ddim_steps=24, 
+                                 unconditional_guidance_scale=14.0, sample=True)
         
         images['samples'] = torch.clamp(images['samples'].detach().cpu() * 0.5 + 0.5, 0., 1.1)
         images['samples']/=(torch.max(torch.abs(images['samples'])))
